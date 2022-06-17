651d79c2efb68ba5d08adaca6ae3eeeafeb85dad
ASUS <bayramov1508@gmail.com>
2022-06-17 18:41:45 +0300

    Working with JOINS in MySQL

---
+++ MySQL/5.MySQL_join.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password="Current-Root-Password",
+    database="test"
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute("ALTER TABLE products ADD FOREIGN KEY (id) REFERENCES users(id)")
+
+sql = "SELECT \
+  users.name AS user, \
+  products.name AS favorite \
+  FROM users \
+  INNER JOIN products ON users.fav = products.id"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
